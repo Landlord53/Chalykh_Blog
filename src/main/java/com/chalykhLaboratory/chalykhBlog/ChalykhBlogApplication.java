f1888fd2401e5d874217c25fc23b7cd652691cda
Nikolai Chalykh <nikolay.chalykh@yandex.ru>
2023-10-26 09:29:51 +0300

    Initial commit

---
+++ src/main/java/com/chalykhLaboratory/chalykhBlog/ChalykhBlogApplication.java
@@ -0,0 +1,13 @@
+package com.chalykhLaboratory.chalykhBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalykhBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalykhBlogApplication.class, args);
+	}
+
+}
